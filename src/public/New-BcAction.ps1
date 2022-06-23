638599acf88ccaa42622d1b842bd5f6e9168d7b4
Anthony Howell <anthony@howell-it.com>
2022-06-23 08:32:24 -0700

    implementing templates into the module

---
+++ src/public/New-BcAction.ps1
@@ -0,0 +1,29 @@
+Function New-BcAction {
+    [cmdletbinding()]
+    param (
+        [string]$TemplateName = 'PowerShellAction',
+        [Parameter(
+            Mandatory,
+            HelpMessage = 'Parent path of the action.'
+        )]
+        [string]$Destination,
+        [switch]$Force
+    )
+    if (-not (Get-Module Plaster -ListAvailable)) {
+        if ($Force.IsPresent) {
+            Install-Module Plaster -Repository PsGallery -Force
+        } else {
+            Write-Warning 'This command requires the Plaster module. Please install or use the -Force switch to automatically isntall.'
+            return
+        }
+    }
+    Invoke-Plaster -TemplatePath $PSScriptRoot\templates\$TemplateName -DestinationPath $Destination
+}
+
+# Argument completer
+$scriptBlock = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+
+    Get-ChildItem $PSScriptRoot\templates\$wordToComplete* | Select-Object -ExpandProperty Name
+}
+Register-ArgumentCompleter -CommandName New-BcAction -ParameterName TemplateName -ScriptBlock $scriptBlock
