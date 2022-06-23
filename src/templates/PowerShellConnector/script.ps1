ccd63161417f577fa2c6553ec1b20f02dcba64d0
Anthony Howell <anthony@howell-it.com>
2022-06-23 08:09:34 -0700

    referencing the ps action to save files

--- src/templates/PowerShellConnector/script.ps1
+++
@@ -1,6 +0,0 @@
-$settings = Get-Content .\settings.json | ConvertFrom-Json
-
-$settings.'Sample String Parameter'
-$settings.'Sample Number Parameter'
-$settings.'Sample Boolean Parameter'
-$settings.'Sample Password Parameter'
