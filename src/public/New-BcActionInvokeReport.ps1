87d483d1383e6cf47c5fe8cd0f71ed2870f13aec
Anthony Howell <anthony@howell-it.com>
2022-08-12 06:57:25 -0700

    adjusting header and spacing

--- src/public/New-BcActionInvokeReport.ps1
+++ src/public/New-BcActionInvokeReport.ps1
@@ -4,33 +4,42 @@ Function New-BcActionInvokeReport {
         [psobject]$InvocationData
     )
 
-    Write-Host "--------------------------------"
-    Write-Host "---Action Invocation Complete---"
-    Write-Host "--------------------------------"
+    $str = @'
+-------------------------------------------------------------
+ ____                           ____ _                 _ 
+| __ ) _ __ __ _ _______ _ __  / ___| | ___  _   _  __| |
+|  _ \| '__/ _` |_  / _ \ '_ \| |   | |/ _ \| | | |/ _` |
+| |_) | | | (_| |/ /  __/ | | | |___| | (_) | |_| | (_| |
+|____/|_|  \__,_/___\___|_| |_|\____|_|\___/ \__,_|\__,_|
+-------------------------------------------------------------
+ Action Invocation Report
+-------------------------------------------------------------
+'@
 
-    Write-Host "Build Process: " -NoNewline
+    Write-Host $str
+
+
+    Write-Host "Build Process:  " -NoNewline
     If ($InvocationData.Build.StdErr.Length -gt 0) {
-        Write-Host "Build Errors:" -ForegroundColor Red
-        Write-Host ($InvocationData.Build.StdErr -join "`n")
+        Write-Host "Errors. View with: '$Out.Build.StdErr'" -ForegroundColor Red
     } else {
         Write-Host "No Errors" -ForegroundColor Green
     }
 
-    Write-Host "Run Process: " -NoNewline
+    Write-Host "Run Process:    " -NoNewline
     If ($InvocationData.Build.StdErr.Length -gt 0) {
-        Write-Host "Run Errors:" -ForegroundColor Red
-        Write-Host ($InvocationData.Run.StdErr -join "`n") -ForegroundColor Red
+        Write-Host "Errors. View with: '$Out.Run.StdErr'" -ForegroundColor Red
     } else {
         Write-Host "No Errors" -ForegroundColor Green
     }
 
-    Write-Host "Action Output: " -NoNewline
-    Write-Host "$($InvocationData.StdOut.Count) lines of stdout." -ForegroundColor Green
+    Write-Host "Action Output:  " -NoNewline
+    Write-Host "$($InvocationData.StdOut.Count) lines of stdout. View with '`$Out.StdOut'" -ForegroundColor Green
 
-    Write-Host "Results: " -NoNewline
+    Write-Host "Results:        " -NoNewline
     If ($InvocationData.Results.Length -gt 0) {
-        Write-Host $InvocationData.Results
+        Write-Host $InvocationData.Results -ForegroundColor Green
     } else {
-        Write-Host "No results."
+        Write-Host "No results." -ForegroundColor Yellow
     }
 }
